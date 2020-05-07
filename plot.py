fcf4fbed08abae75d749d49865b7b7b51b7d12ae
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-07 15:41:32 -0300

    changing plot function

--- plot.py
+++ plot.py
@@ -16,25 +16,33 @@ fig = go.Figure()
 for i in range(len(limits)):
     lim = limits[i]
     
+    loc = []
     lat = []
     lon = []
     num = []
 
     for reg in data:
-      if data[reg]['entregues'] > limits[i][0] and data[reg]['entregues'] < limits[i][1]:
+      if data[reg]['entregues'] >= limits[i][0] and data[reg]['entregues'] < limits[i][1]:
         lat.append(data[reg]['latitude'])
         lon.append(data[reg]['longitude'])
-        num.append(data[reg]['entregues'] / 3)
+        num.append(data[reg]['entregues'])
+        loc.append(reg)
+
+        scale = [nb / 1.5 for nb in num]
+
+        text = ['{0} - {1}'.format(city, quant) for city, quant in zip(loc, num)]
 
     fig.add_trace(go.Scattermapbox(
         lon = lon,
         lat = lat,
         marker = go.scattermapbox.Marker(
-            size = num,
+            size = scale,
             color = colors[i],
-            sizemode = 'area'
+            sizemode = 'area',
         ),
-        name = '{0} - {1}'.format(limits[i][0],limits[i][1])))
+        hoverinfo = 'text',
+        text = text,
+        name = '{0} - {1}'.format(limits[i][0],limits[i][1]))),
 
 
 fig.update_layout(
@@ -43,13 +51,15 @@ fig.update_layout(
         mapbox=dict(
         accesstoken=mapbox_access_token,
         bearing=0,
+        style='outdoors',
         center=go.layout.mapbox.Center(
             lat=-23,
             lon=-45
         ),
         pitch=0,
-        zoom=5
+        zoom=6
     )
     )
 
-fig.show()
+# fig.show()
+fig.write_html('html/SE.html', auto_open=True)
